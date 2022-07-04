540bb5531881a525c0d1bd28b9cb4caa569995ce
Ruobing Shang <shangruobing29@163.com>
2022-07-04 14:41:39 +0800

    Add public security record information

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -156,7 +156,20 @@
         </el-row>
 
         <el-divider style="margin-bottom: 10px" />
-        <el-link href="https://beian.miit.gov.cn/" target="_blank"> 滇ICP备2022003697号-1 </el-link>
+
+        <el-link
+          target="_blank"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=53011402000584"
+          style="margin-right: 20px"
+          :underline="false"
+        >
+          <img src="../assets/beian.png" />
+          滇公网安备 53011402000584号
+        </el-link>
+
+        <el-link href="https://beian.miit.gov.cn/" target="_blank" :underline="false">
+          滇ICP备2022003697号-1
+        </el-link>
         <p style="font-size: 14px; color: #606266">All Rights Reserved Ruobing Shang 版权所有</p>
         <my-robot />
       </el-main>
