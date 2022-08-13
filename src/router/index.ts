696cb0f4357fc75b926318ec848092615251ba29
Ruobing Shang <shangruobing29@163.com>
2022-08-14 00:51:57 +0800

    Fixed the router 404 page bugs.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,113 +1,126 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import store from '../store/index'
 
-type RouteRecord = RouteRecordRaw | any
+declare module 'vue-router' {
+  export interface RouteMeta {
+    requireAuth: boolean
+    isAdmin?: boolean
+    title?: string
+    props?: never
+  }
+}
 
-const routes: Array<RouteRecord> = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    meta: { title: '家' },
+    meta: { requireAuth: false, title: '首页' },
     component: () => import('../views/Home/Index.vue')
   },
   {
     path: '/word/:id',
     name: 'word',
-    meta: { title: '文件预览' },
+    meta: { requireAuth: true, title: '文件预览' },
     component: () => import('../components/WordPreview.vue')
   },
   {
     path: '/register',
     name: 'register',
-    meta: { title: '注册' },
+    meta: { requireAuth: false, title: '注册' },
     component: () => import('../views/Authentication/Register.vue')
   },
   {
     path: '/login',
     name: 'login',
-    meta: { title: '登录' },
+    meta: { requireAuth: false, title: '登录' },
     component: () => import('../views/Authentication/Login.vue')
   },
   {
     path: '/help',
     name: 'help',
-    meta: { title: '帮助' },
+    meta: { requireAuth: false, title: '帮助' },
     component: () => import('../views/Help/Help.vue')
   },
   {
     path: '/docs',
     name: 'docs',
-    meta: { title: '文档' },
-    beforeEnter() {
+    meta: { requireAuth: false, title: '文档' },
+    beforeEnter: () => {
       location.href = 'https://www.infoweaver.cloud/api/docs/'
-    }
+    },
+    // TypeScript Type Check Error: beforeEnter and redirect are not compatible.
+    redirect: ''
   },
   {
-    path: '/content',
+    path: '/content/',
     name: 'content',
+    meta: { requireAuth: true, title: '后台' },
     component: () => import('../views/Content/Index.vue'),
     children: [
       {
         path: '',
         name: 'welcome',
-        meta: { title: '首页' },
+        meta: { requireAuth: true, title: '家' },
         component: () => import('../views/Content/Welcome.vue')
       },
       {
         path: 'chatRobot',
-        meta: { title: '聊天机器人' },
+        meta: { requireAuth: false, title: '聊天机器人' },
         component: () => import('../components/ChatRobot.vue')
       },
       {
         path: 'notice',
-        meta: { subMenu: '通知管理', menuItem: '查看文件' },
+        meta: { requireAuth: true, subMenu: '通知管理', menuItem: '查看文件' },
         component: () => import('../views/NoticeManagement/NoticeList.vue')
       },
       {
         path: 'neo4j',
-        meta: { subMenu: '通知管理', menuItem: '文件查询' },
+        meta: { requireAuth: true, subMenu: '通知管理', menuItem: '文件查询' },
         component: () => import('../views/NoticeManagement/Neo4j.vue')
       },
       {
         path: 'echarts',
-        meta: { subMenu: '通知管理', menuItem: '信息统计' },
+        meta: { requireAuth: true, subMenu: '通知管理', menuItem: '信息统计' },
         component: () => import('../views/NoticeManagement/Statistics.vue')
       },
       {
         path: 'fileList',
-        meta: { subMenu: '文件管理', menuItem: '文件预览' },
+        meta: { requireAuth: true, subMenu: '文件管理', menuItem: '文件预览' },
         component: () => import('../views/FileManagement/FileList.vue')
       },
       {
         path: 'upload',
-        meta: { subMenu: '文件管理', menuItem: '文件上传' },
+        meta: { requireAuth: true, subMenu: '文件管理', menuItem: '文件上传' },
         component: () => import('../views/FileManagement/UploadFileForm.vue')
       },
       {
         path: 'systemInfo',
-        meta: { subMenu: '系统设置', menuItem: '运行状况' },
+        meta: { requireAuth: true, subMenu: '系统设置', menuItem: '运行状况' },
         component: () => import('../views/SystemSettings/SystemInfo.vue')
       },
       {
         path: 'dbInfo',
-        meta: { subMenu: '系统设置', menuItem: '数据库监控' },
+        meta: { requireAuth: true, subMenu: '系统设置', menuItem: '数据库监控' },
         component: () => import('../views/SystemSettings/DBinfo.vue')
       },
       {
         path: 'corpus',
-        meta: { subMenu: '系统设置', menuItem: '问答语料' },
+        meta: { requireAuth: true, subMenu: '系统设置', menuItem: '问答语料' },
         component: () => import('../views/SystemSettings/CorpusList.vue')
       },
       {
         path: 'forbidden',
+        meta: { requireAuth: false, title: '未授权' },
         component: () => import('../views/Exception/Forbidden.vue')
       },
       {
         path: 'help',
+        meta: { requireAuth: true, title: '帮助' },
         component: () => import('../views/Help/Help.vue')
       },
       {
-        path: '/:catchAll(.*)',
+        path: '/:pathMatch(.*)*',
+        meta: { requireAuth: true, title: '未找到' },
         component: () => import('../views/Exception/NotFound.vue')
       }
     ]
@@ -115,41 +128,40 @@ const routes: Array<RouteRecord> = [
   {
     path: '/forbidden',
     name: 'forbidden',
+    meta: { requireAuth: false, title: '未授权' },
     component: () => import('../views/Exception/Forbidden.vue')
   },
   {
-    path: '/:catchAll(.*)',
+    path: '/:pathMatch(.*)*',
     name: 'notFound',
+    meta: { requireAuth: false, title: '未找到' },
     component: () => import('../views/Exception/NotFound.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  strict: true
 })
 
-router.beforeEach((from) => {
+router.beforeEach((to) => {
   const username = localStorage.getItem('username')
   if (username) {
     store.commit('loginSuccess', username)
   }
 
-  const safePage = [
-    'home',
-    'login',
-    'register',
-    'notFound',
-    'help',
-    'test',
-    'word',
-    'docs',
-    'forbidden'
-  ]
-  if (store.getters.isLogin || safePage.indexOf(from.name as string) !== -1) {
-    return true
-  } else {
-    return { name: 'login' }
+  // Auth required and not logged in.
+  if (to.meta.requiresAuth && !store.getters.isLogin) {
+    return { name: 'login', query: { redirect: to.fullPath } }
+  }
+
+  // The page couldn't be found and its path doesn't include "/content".
+  if (to.meta.title === '未找到' && to.path.indexOf('/content') === -1) {
+    // Next time, the page will not jump to the NotFound page.
+    if (to.fullPath !== '/') {
+      return { name: 'notFound' }
+    }
   }
 })
 
